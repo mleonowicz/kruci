549e3187560bd37f2bc4b4711b1c4b18765e37a5
Maksymilian Zadrożny <maxzadrozny1@gmail.com>
2023-11-01 16:53:42 +0100

    main.rs: Add main input loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 mod engine;
+use pest::Parser;
+use pest_derive::Parser;
+use std::io;
+
+#[derive(Parser)]
+#[grammar = "../res/uci.pest"]
+pub struct UCIParser;
 
 fn main() {
-    println!("Workin in progress!");
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim() == "exit" {
+            break;
+        }
+
+        let result = UCIParser::parse(Rule::command, &input);
+        match result {
+            Ok(_) => println!("Valid command: {:#?}", result),
+            Err(e) => println!("Invalid command: {}", e),
+        }
+    }
 }
